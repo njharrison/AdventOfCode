f82d2fd9b113c2f14b1a691742148f9d1502a9f5
Nick Harrison <njharrison@gmail.com>
2020-12-20 20:04:41 +0000

    Day 19 tasks, with Task 2 returning 0 as it's a lengthy operation

---
+++ Day19/RuleNode.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace AdventOfCode.Day19
+{
+    internal class RuleNode
+    {
+        public List<List<RuleNode>> ChildNodes { get; }
+
+        public string Rule { get; set; }
+
+        public RuleNode(string rule)
+        {
+            Rule = rule;
+            ChildNodes = new List<List<RuleNode>>();
+        }
+    }
+}
---
+++ Day19/Task1.cs
@@ -0,0 +1,18 @@
+﻿using AdventOfCode.Tasks;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode.Day19
+{
+    class Task1 : Day19TaskBase, ITask
+    {
+        public ulong Solve(string[] input)
+        {
+            return SolveForInput(input);
+        }
+    }
+}
